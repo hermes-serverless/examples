94984a523ceb5b0b1e5a0c2f197b088109c99e77
tiagonapoli <napoli.tiago96@gmail.com>
2019-09-22 21:45:15 -0300

    Add waiter example

---
+++ cpp/waiter/waiter.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <thread>
+#include <chrono>
+
+int main() {
+  int total, inter;
+  int res = 0;
+  scanf("%d %d", &total, &inter);
+  while(res < total) {
+    printf("Hello %d\n", res);
+    std::this_thread::sleep_for(std::chrono::milliseconds(inter * 1000));  
+    res += inter; 
+  }
+  printf("Hello %d\n", res);
+  return 0;
+}
