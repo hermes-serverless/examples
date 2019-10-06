f4cd9d4030a3929b754693a18d1e087b7ed44737
tiagonapoli <tiago.napoli@vtex.com.br>
2019-10-06 19:35:16 -0300

    Add fflush to waiter

--- cpp/waiter/waiter.cpp
+++ cpp/waiter/waiter.cpp
@@ -8,6 +8,7 @@ int main() {
   scanf("%d %d", &total, &inter);
   while(res < total) {
     printf("Hello %d\n", res);
+	fflush(stdout);
     std::this_thread::sleep_for(std::chrono::milliseconds(inter * 1000));  
     res += inter; 
   }
